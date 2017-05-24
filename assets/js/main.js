67715e7171acfdb020f173fb9c2a56fcc49aa8c6
Orieta Toro <orieta.toro@gmail.com>
2017-05-24 10:56:37 -0400

    desarrollo lift - me falto el js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+/* aqui se suponia que tenia que poner
+las funciones y variables para crear el juego del autito, 
+tenia que ocupar addEventListener para x e y. para variar no me funciono.
+lo siento. 
+Orieta Toro R.
